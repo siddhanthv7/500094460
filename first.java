e0c9168b9239bdd093d9d526d5d547974bb9c0c1
siddhanth verma <siddhanthv10@gmail.com>
2022-09-16 00:58:16 +0530

    Java code file is updated

--- first.java
+++ first.java
@@ -3,13 +3,13 @@ public class Test {
 	public static void main(String args[]){ 
 		Scanner input = null; 
 		try { 
-			System.out.println("Please Enter 10 numbers"); 
+			System.out.println("Please Enter 5 numbers"); 
 			input = new Scanner(System.in); 
-			int sum = 0; 
-			for (int i = 0; i < 10; i += 1){ 
-				sum += input.nextInt(); 
+			int prod = 1; 
+			for (int i = 0; i < 5; i += 1){ 
+				prod *= input.nextInt(); 
 			} 
-			System.out.println("The total sum is: " + sum ); 
+			System.out.println("The product of numbers is: " + prod ); 
 		} catch (Exception e) {  
 			e.printStackTrace(); 
 		} finally { 
